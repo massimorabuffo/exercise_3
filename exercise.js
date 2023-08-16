f3fb422c561f975f5cda07cc82b1d1096e104a0a
Massimo <rabuffo.massimo@gmail.com>
2023-08-16 14:03:52 +0200

    exercise done

---
+++ exercise.js
@@ -0,0 +1,14 @@
+const sum = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const log = function (value) {
+  value = divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5);
+  console.log(value);
+}
+
+log();
